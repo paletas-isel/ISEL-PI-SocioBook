2f4fdac44069febb6e1053823a2b639340c5c54a
João Silvestre <joaopaulosilvestre@hotmail.com>
2011-06-02 16:25:53 +0100

    HouseCleaning - Removido projecto de ASP.NET

--- src/main/csharp/WebServer/Scripts/wall.js
+++
@@ -1,8 +0,0 @@
-﻿function Wall(controller) {
-    $(function () {
-        $(".share > .delete-share").click(function (e) {
-            var share = $(e.target).parent()[0];
-            controller.RemoveShare(share.id);
-        });
-    });
-};
